fdb6952b6798e8afc799c110bfcb6a2ea9475c2c
AmberJing88 <j289xu@uwaterloo.ca>
2022-02-03 19:10:47 -0400

    add new lines in run.py file

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,7 @@ long_description [An example of a step using MLflow and Weights & Biases]: Perfo
 import argparse
 import logging
 import wandb
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -20,9 +21,33 @@ def go(args):
     # particular version of the artifact
     # artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    logger.info("Fetching artifact")
+    artifact - run.use_artifact(args.input_artifact)
+    local_path = artifact.file()
+    
+    logger.info("Reading dataframe")
+    df = pd.read_csv(local_path)
+    
+    # Pre-processing
+    logger.info("Starting pre-processing")
+    df = df.drop_duplicate().reset_index(drop=True)
+    
+    # need to modify with new feature in project
+    df["title"].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + " " + df["song_name"]
+    
+    outfile = args.artifact_name
+    df.to_csv(outfile)
+    artifact = wandb.Artifact(
+        name=args.artifact_name, 
+        type=args.artifact_type,
+        description=args.artifact_description
+    )
+    artifact.add_file(outfile)
+    
+    run.log_artifact(artifact)
+    
 
 
 if __name__ == "__main__":
